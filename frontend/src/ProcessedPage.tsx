8204a82fbca59d3387bab9c9b1decf8d0d0ae95b
isabellaqian <isabellajqian@gmail.com>
2024-09-15 00:29:01 -0400

    first draft pages

--- frontend/src/ProcessedPage.tsx
+++ frontend/src/ProcessedPage.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from './assets/logo_black.png';
 import SymphonicLogo from './assets/symphonic_logo.png';
-import PoweredBySymphonic from './assets/powered_by.png';
 import Tmp from './assets/test_image.png';
 import Record from './assets/record.png';
+import Download from './assets/download.svg';
 import './App.css';
 
 function ProcessedPage() {
@@ -17,16 +16,23 @@ function ProcessedPage() {
   }
   return (
     <div className="container">
-            <img src={Logo} alt="Logo" style={{ width: '118px'}}/>
-            {/* <div>
+            <img src={Logo} alt="Logo" style={{ width: '118px', marginBottom:'60px'}}/>
+            <div className="tag" style={{marginBottom:'24px'}}>
                 <img src={SymphonicLogo} alt="Symphonic Logo" style={{ width: '24px'}}/>
-                <p>Powered by Symphonic</p>
-            </div> */}
-            <img src={PoweredBySymphonic} alt="Powered by Symphonic" style={{ width: '271px'}}/>
+                Powered by Symphonic
+            </div>
             <div className="videoContainer">
                 <img src={Tmp}/>
-                <button className="purpleButton" onClick={handleRecordNewVideo}> <img src={Record} alt="Record button"/>Record video</button>
-                <button className="purpleButton" onClick={handleDownloadVideo}> <img src={Record} alt="Record button"/>Record video</button>
+                <div style={{display:"flex"}}>
+                  <button className="button purple" onClick={handleRecordNewVideo}> 
+                    <img src={Record} alt="Record new video"/>
+                    Record video
+                  </button>
+                  <button className="button black" onClick={handleDownloadVideo}> 
+                    <img src={Download} alt="Download video"/>
+                    Download video
+                  </button>
+                </div>
             </div>
             <h3>Speak, Even When You Can’t.</h3>
         </div>
--- frontend/src/StartPage.tsx
+++ frontend/src/StartPage.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from './assets/logo_black.png';
-// import SymphonicLogo from './assets/symphonic_logo.png';
-import PoweredBySymphonic from './assets/powered_by.png';
+import SymphonicLogo from './assets/symphonic_logo.png';
 import Tmp from './assets/test_image.png';
 import Record from './assets/record.png';
 import './App.css';
@@ -17,14 +15,13 @@ function StartPage() {
     return (
         <div className="container">
             <img src={Logo} alt="Logo" style={{ width: '118px'}}/>
-            {/* <div>
+            <div className="tag" style={{marginBottom:'24px'}}>
                 <img src={SymphonicLogo} alt="Symphonic Logo" style={{ width: '24px'}}/>
-                <p>Powered by Symphonic</p>
-            </div> */}
-            <img src={PoweredBySymphonic} alt="Powered by Symphonic" style={{ width: '271px'}}/>
+                Powered by Symphonic
+            </div>
             <div className="videoContainer">
                 <img src={Tmp}/>
-                    <button className="purpleButton" onClick={goToNextPage}> <img src={Record} alt="Record button"/>Record video</button>
+                    <button className="button purple" onClick={goToNextPage}> <img src={Record} alt="Record button"/>Record video</button>
             </div>
             <h3>Speak, Even When You Can’t.</h3>
         </div>
