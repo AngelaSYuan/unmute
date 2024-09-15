96b8377d28c6210cf75b624eaf12a53f2d25ec9d
isabellaqian <isabellajqian@gmail.com>
2024-09-14 23:57:12 -0400

    cleanup + 2 pages

---
+++ frontend/src/ProcessedPage.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Logo from './assets/logo_black.png';
+import SymphonicLogo from './assets/symphonic_logo.png';
+import PoweredBySymphonic from './assets/powered_by.png';
+import Tmp from './assets/test_image.png';
+import Record from './assets/record.png';
+import './App.css';
+
+function ProcessedPage() {
+  const handleRecordNewVideo = () => {
+    console.log('Record new video');
+  }
+
+  const handleDownloadVideo = () => {
+    console.log('Download video');
+  }
+  return (
+    <div className="container">
+            <img src={Logo} alt="Logo" style={{ width: '118px'}}/>
+            {/* <div>
+                <img src={SymphonicLogo} alt="Symphonic Logo" style={{ width: '24px'}}/>
+                <p>Powered by Symphonic</p>
+            </div> */}
+            <img src={PoweredBySymphonic} alt="Powered by Symphonic" style={{ width: '271px'}}/>
+            <div className="videoContainer">
+                <img src={Tmp}/>
+                <button className="purpleButton" onClick={handleRecordNewVideo}> <img src={Record} alt="Record button"/>Record video</button>
+                <button className="purpleButton" onClick={handleDownloadVideo}> <img src={Record} alt="Record button"/>Record video</button>
+            </div>
+            <h3>Speak, Even When You Can’t.</h3>
+        </div>
+  );
+}
+
+export default ProcessedPage;
