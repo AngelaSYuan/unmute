cc3f4e4f1100fee064791416a31915340acbe531
Angela Yuan <Angelayuan1210@gmail.com>
2024-12-09 15:42:56 -0500

    vercelignore

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,12 +1,12 @@
-import Logo from './assets/logo_black.png';
-import SymphonicLogo from './assets/symphonic_logo.png';
+// import Logo from './assets/logo_black.png';
+// import SymphonicLogo from './assets/symphonic_logo.png';
 
 const Header = () => {
     return (
         <>
-            <img src={Logo} alt="Logo" style={{ width: '118px', marginBottom:'50px'}}/>
+            {/* <img src={Logo} alt="Logo" style={{ width: '118px', marginBottom:'50px'}}/> */}
             <div className="tag" style={{marginBottom:'24px'}}>
-                <img src={SymphonicLogo} alt="Symphonic Logo" style={{ width: '24px'}}/>
+                {/* <img src={SymphonicLogo} alt="Symphonic Logo" style={{ width: '24px'}}/> */}
                 Powered by Symphonic
             </div>
         </>
