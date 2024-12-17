a79c3ba83496d14bafeedd11d7cdd5c749793309
Angela Yuan <Angelayuan1210@gmail.com>
2024-12-16 20:26:08 -0500

    first commit for this version

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StartPage from './StartPage';
-import ProcessedPage from './ProcessedPage';
 import './App.css';
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<StartPage />} />
-        <Route path="/processed" element={<ProcessedPage />} />
       </Routes>
     </Router>
   );
